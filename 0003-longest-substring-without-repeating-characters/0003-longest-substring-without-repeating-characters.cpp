b6fe7ca09c7442ad10ba216ebe300586c50570b5
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-09-22 05:58:22 +0530

    Time: 17 ms (47.17%), Space: 12.5 MB (49.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,16 +4,22 @@ public:
         int n=s.size(),longest=0;
         unordered_map<char,int>mp;
 
-        for(int i=0;i<n;i++){
-            mp.clear();
-            for(int j=i;j<n;j++){
-                mp[s[j]]++;
-                if(mp.size()!=j-i+1){
-                    break;
+        int i=0,j=0;
+
+        while(j<n){
+            mp[s[j]]++;
+
+            while(mp.size()!=j-i+1){
+                mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
                 }
-            longest=max(longest,j-i+1);
+                i++;
             }
+            longest=max(longest,j-i+1);
+            j++;
         }
+        
         return longest;
     }
 };
