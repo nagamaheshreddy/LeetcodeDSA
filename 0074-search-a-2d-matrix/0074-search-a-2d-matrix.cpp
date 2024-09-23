787e3944a0d86cc2b1dbd1d502b26b4ab8a91cd4
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-09-23 06:51:28 +0530

    Time: 0 ms (100%), Space: 12.2 MB (79.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,13 +3,16 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
        int n=matrix.size(),m=matrix[0].size();
 
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+       int i=0,j=m-1;
+       while(i>=0 && i<n && j>=0 && j<m){
             if(matrix[i][j]==target){
                 return true;
+            }else if(matrix[i][j]<target){
+                i++;
+            }else{
+                j--;
             }
-        }
-       } 
+       }
        return false;
     }
 };
