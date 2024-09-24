e89339165514985011c0f1bbcaa320d66b201c2e
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-09-24 06:26:27 +0530

    Time: 12 ms (34.96%), Space: 10.9 MB (67.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -17,6 +17,7 @@ public:
         while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
+
             if(slow==fast)return true;
         }
         return false;
