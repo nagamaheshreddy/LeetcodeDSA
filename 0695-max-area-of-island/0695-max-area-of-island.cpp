7ecf47dccbecc40059a8f646ef0d4bd766a0a443
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-09-28 07:30:17 +0530

    Time: 11 ms (79.1%), Space: 26.7 MB (98.49%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
 
-    int  dfs(int i,int j,int rows,int cols,vector<vector<int>>& grid,vector<vector<int>>& vis){
-            if(i<0 || i>=rows ||j<0 || j>=cols || vis[i][j]==1 || grid[i][j]==0)return 0;
+    int  dfs(int i,int j,int rows,int cols,vector<vector<int>>& grid){
+            if(i<0 || i>=rows ||j<0 || j>=cols || grid[i][j]==0)return 0;
 
-            vis[i][j]=1;
+            grid[i][j]=0;
 
-            int a=dfs(i+1,j,rows,cols,grid,vis);
-            int b=dfs(i-1,j,rows,cols,grid,vis);
-            int c=dfs(i,j+1,rows,cols,grid,vis);
-            int d=dfs(i,j-1,rows,cols,grid,vis);
+            int a=dfs(i+1,j,rows,cols,grid);
+            int b=dfs(i-1,j,rows,cols,grid);
+            int c=dfs(i,j+1,rows,cols,grid);
+            int d=dfs(i,j-1,rows,cols,grid);
 
             return 1+a+b+c+d;
         }
@@ -17,14 +17,13 @@ public:
         int rows=grid.size();
         int cols=grid[0].size();
 
-        vector<vector<int>>vis(rows,vector<int>(cols,-1));
         int count=0;
         int maxArea=0;
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(grid[i][j]==1 && vis[i][j]==-1){
-                    count=dfs(i,j,rows,cols,grid,vis);
+                if(grid[i][j]==1){
+                    count=dfs(i,j,rows,cols,grid);
                     maxArea=max(maxArea,count);
                 }
             }
