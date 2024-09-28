d9384fb043105a9fe7f7180cc49136d550128352
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-09-28 10:07:00 +0530

    Time: 23 ms (18.94%), Space: 19.2 MB (21.37%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prereq) {
+        vector<int>adj[n];
+
+        for(auto course:prereq){
+            int u=course[0];
+            int v=course[1];
+            adj[v].push_back(u);
+        }
+        vector<int>inorder(n,0);
+        for(int i=0;i<n;i++){
+            for(auto course:adj[i]){
+                inorder[course]++;
+            }
+        }
+        vector<int>topo;
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(inorder[i]==0){
+                q.push(i);
+            }
+        }
+        
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto child:adj[node]){
+                inorder[child]--;
+                if(inorder[child]==0)q.push(child);
+            }
+        }
+        return topo.size()==n;
+    }
+};
