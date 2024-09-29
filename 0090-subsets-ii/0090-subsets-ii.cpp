f50f3111922703d3305403ecac9c055dcfb423d8
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-09-29 09:47:06 +0530

    Time: 4 ms (48.2%), Space: 8.9 MB (79.57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    void solve(int ind,vector<int>&subset,vector<int>&nums,vector<vector<int>>&ans,set<vector<int>>&s){
-        if(ind==nums.size()){
-            s.insert(subset);
-            return;
+    void solve(int start,vector<int>&subset,vector<int>&nums,vector<vector<int>>&ans){
+        ans.push_back(subset);
+
+        for(int i=start;i<nums.size();i++){
+            if(i!=start && nums[i]==nums[i-1])continue;
+
+            subset.push_back(nums[i]);
+            solve(i+1,subset,nums,ans);
+            subset.pop_back();
         }
-
-        solve(ind+1,subset,nums,ans,s);
-
-        subset.push_back(nums[ind]);
-        solve(ind+1,subset,nums,ans,s);
-        subset.pop_back();
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>subset;
-        set<vector<int>>s;
         sort(nums.begin(),nums.end());
 
-        solve(0,subset,nums,ans,s);
-
-        for(auto subset:s){
-            ans.push_back(subset);
-        }
+        solve(0,subset,nums,ans);
 
         return ans;
     }
