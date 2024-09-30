127b9745e24fa0b39e1058b3229d2ad4cf90a8df
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-09-30 19:55:59 +0530

    Time: 38 ms (83.88%), Space: 51 MB (93.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    bool solve(int ind,vector<int>&nums,vector<int>&memo){
-        if(ind>=nums.size()-1)return true;
-
-        if(memo[ind]!=-1)return memo[ind];
-
-        int jumps=nums[ind];
-        for(int i=1;i<=jumps;i++){
-            if(solve(i+ind,nums,memo)){
-                return memo[ind]=true;
-            }
-        }
-        return memo[ind]=false;
-    }
     bool canJump(vector<int>& nums) {
        int n=nums.size();
        int goal=n-1;
