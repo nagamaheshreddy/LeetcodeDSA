1c2ff001acf9c0772d5a8db8d4417f8ba3863dd8
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-10-03 04:44:59 +0530

    Time: 0 ms (100%), Space: 7.7 MB (9.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -12,11 +12,16 @@ public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
         dp[0]=dp[1]=1;
+        if(n==0 || n==1)return 1;
+        
+        int curr,prev1=1,prev2=1;
 
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
         }
-        return dp[n];
+        return curr;
         
     }
 };
