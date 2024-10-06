662e54c7a6b5be5973b253e3fb99ac968296e726
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-10-06 05:47:19 +0530

    Time: 35 ms (42.57%), Space: 19.4 MB (79.57%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
 
     int dfs(int i,int j,int n,int m,long long prev,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        if(i<0 || i>=n || j<0 || j>=m )return 0;
-        if(prev<=matrix[i][j])return 0;
+        if(i<0 || i>=n || j<0 || j>=m || prev<=matrix[i][j])return 0;
+
         if(dp[i][j]!=-1)return dp[i][j];
 
         int up=dfs(i-1,j,n,m,matrix[i][j],matrix,dp);
         int down=dfs(i+1,j,n,m,matrix[i][j],matrix,dp);
         int left=dfs(i,j-1,n,m,matrix[i][j],matrix,dp);
         int right=dfs(i,j+1,n,m,matrix[i][j],matrix,dp);
-
 
         return dp[i][j]=1+max(up,max(down,max(left,right))); 
     }
