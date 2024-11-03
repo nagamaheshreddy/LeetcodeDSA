9f883e4d660bbd29bcb555aa76fc8a9e17072532
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-11-03 15:42:01 +0530

    Time: 0 ms (100%), Space: 8.7 MB (26.58%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        map<char,vector<string>>mp;
-        vector<string>ans;
-        if(digits.size()==0)return ans;
-        
-        mp['2']={"a","b","c"};
-        mp['3']={"d","e","f"};
-        mp['4']={"g","h","i"};
-        mp['5']={"j","k","l"};
-        mp['6']={"m","n","o"};
-        mp['7']={"p","q","r","s"};
-        mp['8']={"t","u","v"};
-        mp['9']={"w","x","y","z"};
-        
-        vector<string>prev=mp[digits[0]];
-        
-        for(int i=1;i<digits.size();i++){
-            vector<string>curr=mp[digits[i]];
-            vector<string>ans;
-            
-            for(int j=0;j<prev.size();j++){
-                for(int k=0;k<curr.size();k++){
-                    ans.push_back(prev[j]+curr[k]);
-                }
-            }
-            prev=ans;
+
+    void dfs(int ind,string digits,string currStr,vector<string>&ans,unordered_map<char,string>&mp){
+        if(currStr.size()==digits.size()){
+            ans.push_back(currStr);
+            return;
         }
-        return prev;
+
+        for(char ch:mp[digits[ind]]){
+            dfs(ind+1,digits,currStr+ch,ans,mp);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0)return {};
+
+        unordered_map<char,string>mp;
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
+
+        vector<string>ans;
+        string currStr="";
+        dfs(0,digits,currStr,ans,mp);
+
+        return ans;
     }
 };
