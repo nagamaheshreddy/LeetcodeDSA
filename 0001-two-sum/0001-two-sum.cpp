91a4e00c6089eeca717847834914caae32bc5310
MAHESH REDDY <40000936+nagamaheshreddy@users.noreply.github.com>
2024-12-23 14:45:44 +0530

    Time: 3 ms (77.4%), Space: 15 MB (15.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,14 +4,15 @@ public:
         vector<int>ans;
         int n=nums.size();
 
+        map<int,int>mp;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+            int req=target-nums[i];
+            if(mp.find(req)!=mp.end()){
+                ans.push_back(mp[req]);
+                ans.push_back(i);
+                return ans;
             }
+            mp[nums[i]]=i;
         }
         return ans;
     }
